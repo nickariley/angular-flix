b8fedcb3852d428ccc35f63642cec163b59b2106
nickariley <nickariley@gmail.com>
2020-09-03 14:40:35 -0500

    list toggle savetolist

--- src/app/list-toggle/list-toggle.component.ts
+++ src/app/list-toggle/list-toggle.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-list-toggle',
@@ -7,12 +8,12 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class ListToggleComponent implements OnInit {
   @Input() movie;
-  constructor() { }
+  constructor(private movieService: MovieService) { }
 
   ngOnInit() {
   }
-  saveMovieToList(){
-    
+  saveMovieToList() {
+    this.movieService.saveToList(this.movie);
   }
 
 }
